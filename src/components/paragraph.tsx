cf876559006b91ebf8e3bc4009661c8f5f7b45e4
Random Long Int <pierrelouislet@gmail.com>
2024-02-07 16:36:23 -0500

    quick fix

--- src/components/paragraph.tsx
+++ src/components/paragraph.tsx
@@ -4,7 +4,10 @@ interface ParagraphPros extends React.HTMLProps<HTMLParagraphElement> {}
 
 export function Paragraph({ children, className, ...props }: ParagraphPros) {
   return (
-    <p className={cn("text-sm font-normal md:text-lg", className)} {...props}>
+    <p
+      className={cn("text-sm font-normal leading-7 md:text-lg", className)}
+      {...props}
+    >
       {children}
     </p>
   );
